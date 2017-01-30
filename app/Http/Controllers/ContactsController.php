de74a6f29d5ea771c552d9973fa05866d1d244ea
Calvin Mendes Cattaneo <calvin.cattaneo@gmail.com>
2017-01-30 15:36:15 -0200

    Display all contacts Data with Pagination

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Contact;
 
 class ContactsController extends Controller
 {
     public function index()
     {
-        return view('contacts.index');
+        $contacts = Contact::paginate(5);
+        return view('contacts.index', compact('contacts'));
     }
 }
