10e9bcdce6da695145ef681d898f7c728ada86d0
Calvin Mendes Cattaneo <calvin.cattaneo@gmail.com>
2017-02-06 15:48:31 -0200

    autocomplete search

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,4 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::post('groups/store', ['uses' => 'GroupsController@store', 'as' => 'groups.store']);
+Route::get('contacts/autocomplete', ['uses' => 'ContactsController@autocomplete', 'as' => 'contacts.autocomplete']);
 Route::resource('contacts', 'ContactsController');
--- resources/views/contacts/form.blade.php
+++ resources/views/contacts/form.blade.php
@@ -59,7 +59,7 @@
             <div class="input-group">
               <input type="text" name="new_group" id="new_group" class="form-control">
               <span class="input-group-btn">
-                <a href="#" class="btn btn-default">
+                <a href="#" id="add-new-btn" class="btn btn-default">
                   <i class="glyphicon glyphicon-ok"></i>
                 </a>
               </span>
@@ -95,3 +95,58 @@
     </div>
   </div>
 </div>
+
+@section('form-script')
+    <script>
+    $("#add-new-group").hide();
+    $('#add-group-btn').click(function () {
+      $("#add-new-group").slideToggle(function() {
+        $('#new_group').focus();
+      });
+      return false;
+    });
+
+    $("#add-new-btn").click(function() {
+        var newGroup = $("#new_group");
+        var inputGroup = newGroup.closest('.input-group');
+
+        $.ajax({
+            url: "{{ route("groups.store") }}",
+            method: 'post',
+            data: {
+                name: $("#new_group").val(),
+                _token: $("input[name=_token]").val()
+            },
+            success: function(group) {
+                if (group.id != null) {
+                    inputGroup.removeClass('has-error');
+                    inputGroup.next('.text-danger').remove();
+
+                    var newOption = $('<option></option>')
+                        .attr('value', group.id)
+                        .attr('selected', true)
+                        .text(group.name);
+                        
+                    $("select[name=group_id]")
+                        .append( newOption );
+
+                    newGroup.val("");
+                }
+            },
+            error: function(xhr) {
+                var errors = xhr.responseJSON;
+                var error = errors.name[0];
+                if (error) {
+
+
+                    inputGroup.next('.text-danger').remove();
+
+                    inputGroup
+                        .addClass('has-error')
+                        .after('<p class="text-danger">' + error + '</p>');
+                }
+            }
+        });
+    });
+    </script>
+@endsection
